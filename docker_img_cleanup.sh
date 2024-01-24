a288e822517bdf9d9089029e5c49c4b4075626b2
BwithE <144924113+BwithE@users.noreply.github.com>
2024-01-23 20:53:20 -0500

    Update docker_img_cleanup.sh

--- docker_img_cleanup.sh
+++ docker_img_cleanup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+clear
+# Checks to verify that the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "THIS SCRIPT NEEDS TO BE RUN AS ROOT."
+   echo "EX: sudo ./boot-tools.sh"
+   exit 1
+fi
+clear
 # run this script after you've stopped all previous containers
 
 # Get the list of image IDs using "docker images"

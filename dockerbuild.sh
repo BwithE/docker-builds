0542629a744cea34b6b13cca17e9645a3f3e59f9
BwithE <144924113+BwithE@users.noreply.github.com>
2024-01-23 20:52:42 -0500

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Checks to verify that the script is running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "THIS SCRIPT NEEDS TO BE RUN AS ROOT."
+   echo "EX: sudo ./boot-tools.sh"
+   exit 1
+fi
+
+clear
 # sets the number of containers you want to create
 read -p "How many containers would you like to test out?: " num_containers
 #num_containers=5
